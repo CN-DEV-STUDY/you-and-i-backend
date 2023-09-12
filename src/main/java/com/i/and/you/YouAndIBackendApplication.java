7d730337592c596244777e71cdab40ca4aaba9db
nohyunha95 <nohyunha95@gmail.com>
2023-09-12 22:50:54 +0900

    first commit

---
+++ src/main/java/com/i/and/you/YouAndIBackendApplication.java
@@ -0,0 +1,13 @@
+package com.i.and.you;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouAndIBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YouAndIBackendApplication.class, args);
+    }
+
+}
