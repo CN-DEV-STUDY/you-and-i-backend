b6ffbceeacb63193c06c40fa2e6bcc28284977fa
suhyeon chae <86937649+chaesuhyeon@users.noreply.github.com>
2023-10-19 21:30:06 +0900

    Update Dockerfile

--- src/main/java/com/i/and/you/common/web/WebMvcConfig.java
+++ src/main/java/com/i/and/you/common/web/WebMvcConfig.java
@@ -1,6 +1,7 @@
 package com.i.and.you.common.web;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,6 +12,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @RequiredArgsConstructor
 public class WebMvcConfig implements WebMvcConfigurer {
+    @Value("${cors.allowedOrigins}")
+    private String allowedOrigin;
+
     private final long MAX_AGE_SECS = 3600;
 
     @Override
@@ -18,7 +22,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
         // 모든 경로에 대해
         registry.addMapping("/**")
                 // Origin이 localhost:3000에 대해
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("http://localhost:5173", allowedOrigin)
                 // GET, POST, PUT, PATCH, DELETE, OPTIONS 메서드를 허용한다.
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
