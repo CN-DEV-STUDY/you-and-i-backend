c1920e3201b0d90ec6baf6f92e1a012a4fccfb27
suhyeon chae <86937649+chaesuhyeon@users.noreply.github.com>
2023-10-22 20:20:16 +0900

    Update docker-deploy.yml

--- src/main/java/com/i/and/you/global/web/WebMvcConfig.java
+++ src/main/java/com/i/and/you/global/web/WebMvcConfig.java
@@ -21,8 +21,8 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         // 모든 경로에 대해
         registry.addMapping("/**")
-                // Origin이 localhost:3000에 대해
-                .allowedOrigins("http://localhost:5173", allowedOrigin)
+                // Origin이 localhost:5173에 대해
+                .allowedOrigins(allowedOrigin, "http://localhost:5173")
                 // GET, POST, PUT, PATCH, DELETE, OPTIONS 메서드를 허용한다.
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
