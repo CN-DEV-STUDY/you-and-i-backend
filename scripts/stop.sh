a01af32041541097d795875555f8aa0283e1e9f1
chaesuhyeon <ddd8177@gmail.com>
2023-10-23 20:58:11 +0900

    Update stop.sh

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,4 +1,5 @@
 CONTAINER_NAME=you-and-i-backend
+IMAGE_NAME=ddd8177/you-and-i-backend
 
 # 도커 컨테이너가 있는지 확인 (-a 옵션으로 정지된 컨테이너도 확인)
 RUNNING_CONTAINER_ID=$(docker ps -aq --filter "name=$CONTAINER_NAME")
@@ -8,4 +9,5 @@ echo "실행중인 컨테이너 ID: $RUNNING_CONTAINER_ID"
 if [ -n "$RUNNING_CONTAINER_ID" ]; then
   echo "이전 도커 컨테이너 종료 및 삭제합니다"
   docker stop $CONTAINER_NAME && docker rm $CONTAINER_NAME
+  docker rmi IMAGE_NAME
 fi
